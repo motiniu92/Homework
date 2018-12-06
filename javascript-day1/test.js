2102f73954800c29cac935e6e3e11b9d3d08d520
motiniu92 <43033677+motiniu92@users.noreply.github.com>
2018-12-06 22:50:44 +0600

    u

---
+++ javascript-day1/test.js
@@ -0,0 +1,18 @@
+document.write("Hello world"+"<br/>");
+document.write("<br/>");
+
+
+var x= 10;
+var y= 15;
+var result = x + y;
+document.write("result= "+result+"<br/>");
+
+var str = "Hello world"+"<br/>";
+document.write(str);
+
+
+var multi = x * y;
+console.log("=========result========= "+multi);
+
+var decleration="10 flags";
+//Single line Comments in Js
